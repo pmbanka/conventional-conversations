a970a0d64abbb742a1d304aab1b309274b9389e7
souldzin <souldzin@souldzin.com>
2021-05-16 16:36:02 -0500

    Upgrade to tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        gray: {
-          100: "#f5f5f5",
-          200: "#eeeeee",
-          300: "#e0e0e0",
-          400: "#bdbdbd",
-          500: "#9e9e9e",
-          600: "#757575",
-          700: "#616161",
-          800: "#424242",
-          900: "#212121",
-        },
+        gray: colors.trueGray,
         pink: {
           600: "#ce017b",
         },
